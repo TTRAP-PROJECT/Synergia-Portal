69e8cc79ade1e7ebc62797e6a0b26edbcf5d7d46
tombury <tombury59@hotmail.com>
2024-03-22 17:00:19 +0100

    ajout services

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CINEMA;
+use App\Models\COVOITURAGE;
+use App\Models\EVENEMENTSPORTIF;
 use App\Models\SERVICE;
 use Illuminate\Http\Request;
 
@@ -10,6 +13,13 @@ class ServicesController extends Controller
     public function services()
     {
         $services = SERVICE::all();
-        return view('services', array('services' => $services));
+        $evenementsSportif = EVENEMENTSPORTIF::with('sport')->get();
+        $evenementCinema = CINEMA::all();
+        $covoiturages=Covoiturage::all();
+        return view('services',
+            array('services' => $services,
+                'evenementsSportif'=>$evenementsSportif,
+                'evenementCinema'=>$evenementCinema,
+                'covoiturages'=>$covoiturages));
     }
 }
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,7 +2,60 @@
     <div class="form">
 
     </div>
-    <div>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
 
-    </div>
+                            {{-- Afficher les événements --}}
+                            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                                @foreach ($evenementCinema as $evenementCinemaData)
+                                    <div class="bg-red-500 rounded-lg shadow-md p-4 flex flex-col">
+
+                                        <div class="mt-4">
+                                            <h2 class="text-lg font-bold text-white">{{ $evenementCinemaData->NOMFILM }}</h2>
+                                            <p class="text-gray-200">{{ $evenementCinemaData->LIEUFILM }}</p>
+                                            <p class="text-gray-200">Le {{ $evenementCinemaData->DATEHEUREFILM }}</p>
+                                        </div>
+                                        <div class="flex items-center justify-end">
+                                            <img src="/images/cinema.png" class="w-[70px] h-[70px] rounded-full bg-red-400 p-1">
+                                        </div>
+                                    </div>
+
+                                @endforeach
+                                @foreach ($evenementsSportif as $evenementsSportifData)
+                                        <div class="bg-blue-400 rounded-lg shadow-md p-4 flex flex-col">
+
+                                            <div class="mt-4">
+                                                <h2 class="text-lg font-bold text-white">{{ $evenementsSportifData->sport->LIBELLESPORT }}</h2>
+                                                <p class="text-gray-200">{{ $evenementsSportifData->LIEUEVENT }}</p>
+                                                <p class="text-gray-200">Le {{ $evenementsSportifData->DATEEVENT }}</p>
+                                            </div>
+                                            <div class="flex items-center justify-end">
+                                                <img src="/images/des-sports.png" class="w-[70px] h-[70px] rounded-full bg-blue-300 p-1">
+                                            </div>
+                                        </div>
+                                @endforeach
+                                @foreach ($covoiturages as $covoituragesData)
+                                        <div class="bg-green-400 rounded-lg shadow-md p-4 flex flex-col">
+
+                                            <div class="mt-4">
+                                                <h2 class="text-lg font-bold text-white">Convoiturage</h2>
+                                                <p class="text-gray-200">{{ $covoituragesData->LIEUDEPART }}-{{ $covoituragesData->LIEUARRIVE }}</p>
+                                                <p class="text-gray-200">Le {{ $covoituragesData->DATECOVOIT }}</p>
+                                            </div>
+                                            <div class="flex items-center justify-end">
+                                                <img src="/images/volant.png" class="w-[70px] h-[70px] rounded-full bg-green-300 p-1">
+                                            </div>
+                                        </div>
+
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
 </x-app-layout>
