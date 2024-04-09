e115ce5b3ff6234ef8f034d33be1bbed14a9f56a
Teddy RIALET <teddy@rialet.fr>
2024-04-09 11:52:19 +0200

    maj filtre

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -14,7 +14,7 @@
     @endif
     <div class="grid grid-cols-3 gap-4">
         <div>
-            <div class='mx-5 w-1/2'>
+            <div class='mx-5 mt-10 w-1/2'>
                 <div class="relative flex items-center w-50 h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
                     <div class="grid place-items-center h-full w-12 text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
